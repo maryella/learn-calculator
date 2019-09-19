05ffbaf48907a8373fc28c46a26ed15bfbd40536
Mary Ella Hill <hillmaryella@gmail.com>
2019-09-19 15:03:34 -0400

    First Edits - number & operators show in window when pushed

--- js/script.js
+++ js/script.js
@@ -9,9 +9,26 @@ const input = document.getElementById("input"), // input/output button
 let resultDisplayed = false; // flag to keep an eye on what output is displayed
 
 // numbers is a NodeList object, we need to make it into an array first, then we can map through it...
+numbers.forEach(function(number){
+    number.addEventListener("click", function(e){
+        input.innerHTML += this.innerHTML;
+    })
+})
 
 // adding click handlers to the calculation buttons
-
+operators.forEach(function(operator){
+    operator.addEventListener("click", function(e){
+        input.innerHTML += this.innerHTML;
+    })
+})
 // on click of 'equal' button, perform the mathematical operation
 
 // clear the input on press of clear
+clear.addEventListener("dblclick", function(e){
+    input.innerHTML = "0"
+})
+
+clear.addEventListener("click", function(e){
+    
+})
+
